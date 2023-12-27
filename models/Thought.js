1394a1defc5d4268cf8abe85d819e9e87c6c1a12
Baljot Hansi <baljotshansi@gmail.com>
2023-12-27 14:07:14 -0500

    Defined thought schema.

---
+++ models/Thought.js
@@ -0,0 +1,61 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Schema.Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      max_length: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (date) => date.toLocaleDateString('en-us')
+    },
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      max_length: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (date) => date.toLocaleDateString('en-us')
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [ reactionSchema ]
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true
+    }
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(() => this.reactions.length);
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
