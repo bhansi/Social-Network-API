058c54f6fa1872deeff55c09cfb45d5167b0513a
Baljot Hansi <baljotshansi@gmail.com>
2024-01-05 08:39:25 -0400

    Added route to get one user using id param.

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,9 +3,26 @@ const { User } = require('../models');
 module.exports = {
   getAllUsers: async (req, res) => {
     try {
-      res.json(await User.find());
+      res.status(200).json(await User.find());
     }
     catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  },
+  getOneUser: async (req, res) => {
+    try {
+      const user = await User.find({ _id: req.params.id });
+
+      if (!user) {
+        res.status(404).json({ message: 'No user found with that id.' });
+      }
+      else {
+        res.status(200).json(user);
+      }
+    }
+    catch (err) {
+      console.error(err);
       res.status(500).json(err);
     }
   }
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,3 +1,11 @@
 const router = require('express').Router();
+const {
+  getAllUsers,
+  getOneUser
+} = require('../../controllers/userController');
+
+router.route('/').get(getAllUsers);
+
+router.route('/:id').get(getOneUser);
 
 module.exports = router;
