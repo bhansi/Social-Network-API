bc54d3f4e356975564f63a631deb26a90f9d46d3
Baljot Hansi <baljotshansi@gmail.com>
2024-01-05 21:39:59 -0400

    Added route to get thought by id.

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -9,5 +9,21 @@ module.exports = {
       console.error(err);
       res.status(500).json(err);
     }
-  }
+  },
+  getOneThought: async (req, res) => {
+    try {
+      const thought = await Thought.findOne({ _id: req.params.id })
+
+      if (!thought) {
+        res.status(404).json({ message: 'No thought found with that id.' });
+      }
+      else {
+        res.status(200).json(thought);
+      }
+    }
+    catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  },
 };
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
 const {
-  getAllThoughts
+  getAllThoughts, getOneThought
 } = require('../../controllers/thoughtController');
 
 router.route('/').get(getAllThoughts);
+
+router.route('/:id').get(getOneThought);
 
 module.exports = router;
